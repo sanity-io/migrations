956f7d18f53115a041b109d961f387747ea09f5f
Espen Hovlandsdal <espen@hovlandsdal.com>
2017-09-22 11:30:13 +0200

    Use export endpoint to retrieve all documents

--- fetchAllDocuments.js
+++ fetchAllDocuments.js
@@ -1,15 +1,14 @@
-async function fetchAllDocuments(client) {
-  const limit = 250
-  let docs = []
-  let batch
-  let offset = 0
-  do {
-    batch = await client.fetch(`* | order(_id) [${offset}...${offset + limit}]`)
-    docs = docs.concat(batch)
-    offset += limit
-  } while (batch.length)
-
-  return docs
+function fetchAllDocuments(client) {
+  return client
+    .request({
+      url: `/data/export/${client.config().dataset}`
+    })
+    .then(res =>
+      res
+        .trim()
+        .split('\n')
+        .map(line => JSON.parse(line))
+    )
 }
 
 module.exports = fetchAllDocuments
